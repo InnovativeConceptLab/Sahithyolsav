2ace5c26139866c65736ad866795538fdb921a15
sunu4ever <mbabusunu@gmail.com>
2014-08-27 00:35:23 +0530

    script for participant table alter

---
+++ Scripts/15.sql
@@ -0,0 +1,12 @@
+  alter table  tbl_Participant
+  add vchImagePath varchar(50) null
+    
+  alter table  tbl_Participant
+  add intAge int
+    
+  alter table  tbl_Participant
+  add vchCampusName varchar(50) null
+  
+    
+  alter table  tbl_Participant
+  add vchCourse varchar(50) null
